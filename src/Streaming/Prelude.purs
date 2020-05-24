795c7177e54c721c00945c5cf4f9207a2fd2146f
Woodson Delhia <woodsondelhia88@gmail.com>
2020-05-24 20:35:03 +0900

    added stream transformers and producer

--- src/Streaming/Prelude.purs
+++ src/Streaming/Prelude.purs
@@ -5,6 +5,8 @@ module Streaming.Prelude (
 
   -- * producers 
   , yield 
+  , nats
+  , unfoldr
 
   -- * consumers 
   , print
@@ -12,13 +14,22 @@ module Streaming.Prelude (
   -- * sum and compose manipulation
   , sum
   , sum_
+
+  -- * stream transformers
+  , take 
+
+  -- * folds
+  , fold
+  , fold_ 
   )
   where 
 
 import Prelude
 
+import Data.Either (Either(..))
 import Data.Functor.Of (Of(..)) as Of
 import Data.Functor.Of (Of, (:>))
+import Data.Tuple.Nested (type (/\), (/\))
 import Effect.Class (class MonadEffect)
 import Effect.Class.Console as Console
 import Streaming.Internal (Stream(..))
@@ -54,3 +65,35 @@ fold step begin done str =  fold_eval str begin
 
 fold_ :: forall m r x a b. Monad m => (x -> a -> x) -> x -> (x -> b) -> Stream (Of a) m r -> m b
 fold_ step begin done = map (\(a :> _) -> a) <<< fold step begin done
+
+unfoldr :: forall m s a r. 
+  Monad m 
+  => (s -> m (Either r (a /\ s))) 
+  -> s -> Stream (Of a) m r
+unfoldr step = loop 
+  where
+    loop x = Effect do
+      e <- step x
+      case e of
+        Left r      -> pure $ Return r
+        Right (a /\ s) -> pure $ Step (a :> loop s)
+
+take :: forall m f r. 
+  Monad m 
+  => Functor f 
+  => Int 
+  -> Stream f m r 
+  -> Stream f m Unit
+take n0 _ | n0 <= 0 = pure unit 
+take n0 stream = eval n0 stream where
+  eval 0 _ = pure unit 
+  eval n p =
+    case p of
+      Step fas -> Step $ map (eval (n-1)) fas
+      Effect m -> Effect $ map (eval n) m
+      Return _ -> Return unit 
+
+nats :: forall m r. Monad m => Stream (Of Int) m r 
+nats = do 
+  flip unfoldr 0 \n -> do 
+    pure $ Right (n /\ (n + 1))
